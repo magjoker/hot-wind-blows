3e568ebf6cf9dd9ac1484d5873cbdcab66ccb174
Jared Aragon <jaredaragon7@gmail.com>
2023-06-08 15:42:40 -0600

    portfolio now shows photos of projects

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Navigator from "./components/Navigator";
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <header>
+        <Header />
+    </header>
+    <section>
+        <Navigator />
+    </section>
+    <footer>
+      <Footer />
+    </footer>
+    </>
+  )
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,14 @@
+import React from 'react';
 import '../App.css';
+
 
 function Header () {
     return(
-        <div className="nav-bar">
-            <h1>Jared Aragon Developer Portfolio</h1>
+        <div className="header-txt">
+            <p className="header-txt1">“We delight in the beauty of the butterfly,</p>
+            <p>but rarely admit the changes it has gone through</p>
+            <p>to achieve that beauty.”</p>
+            <p> ― Maya Angelou</p>
         </div>
     );
 }
